79372ca990a5ff56d7756149517b8bf28b7166b6
Shivamyadav07 <shivamvnsindia@gmail.com>
2023-07-21 15:37:58 +0530

    created the header and implemented all the functionalities

--- index.js
+++ index.js
@@ -241,8 +241,9 @@ var data = [
     }
 
 ];
+var copyData = [...data];
 
-
+// creating function to get group color and parcel color dynamically
 function getGroupColor(group) {
     switch (group) {
         case 'Mumbai':
@@ -256,18 +257,193 @@ function getGroupColor(group) {
     }
 }
 
-function renderParcel() {
+var selectedData = "";
+var selectedIndex = "";
+
+// function for creating new header group
+function createHeaderGroup(currgroup, count) {
+    let group = document.createElement("div");
+    group.textContent = currgroup;
+    group.classList.add("group", currgroup);
+    group.style.width = count * 140 + "px";
+    return group;
+}
+
+// this is the function for rendering the application according to data
+function renderParcel(parcelData) {
     let parcel_group = document.querySelector(".parcel_group");
+    parcel_group.innerHTML = "";
+    let currentGroup = null;
+    let count = 0;
+
     let display_parcel = document.querySelector(".display_parcel");
-    data.map((e) => {
+    display_parcel.innerHTML = "";
+
+    let parcel_text = document.querySelector(".selected_parcel");
+    parcel_text.innerHTML = "";
+
+    selectedData = "";
+    selectedIndex = "";
+
+    document.querySelector("input").value = "";
+    document.querySelector("select").value = "";
+
+    parcelData.map((e, index) => {
+
+        // logic for when to create new group and when to just increment the count
+        if (e.group !== currentGroup) {
+            if (currentGroup !== null) {
+                let groupDiv = createHeaderGroup(currentGroup, count);
+                parcel_group.appendChild(groupDiv)
+            }
+            currentGroup = e.group;
+            count = 1;
+        } else {
+            count++;
+        }
+
+        // creation of parcel div and appending it into the display_parcel container 
         let parcelDiv = document.createElement("div");
-        parcelDiv.setAttribute("class","parcelDiv");
-        parcelDiv.innerText = e.sequence;
-        parcelDiv.style.backgroundColor = getGroupColor(e.group);
-        parcelDiv.addEventListener("click",function(){
-            let parcel=document.querySelector(".parcel").innerText=e.name;
+        parcelDiv.setAttribute("class", "parcelDiv");
+
+        let name = document.createElement("div");
+        name.innerText = e.name;
+
+        let sequence = document.createElement("div");
+        sequence.innerText = e.sequence
+        sequence.style.backgroundColor = getGroupColor(e.group);
+        sequence.setAttribute("class", "sequence_div");
+
+        parcelDiv.append(name, sequence);
+
+        // addition of event for selecting particular parcel and show the selected parcel
+        parcelDiv.addEventListener("click", function () {
+            if (parcelDiv.classList.contains("selected")) {
+                parcelDiv.classList.remove("selected");
+                document.querySelector(".selected_parcel").textContent = ""
+                selectedData = "";
+                selectedIndex = "";
+                parcelDiv.style.border = ""
+            }
+            else {
+                let selectedParcel = document.querySelector(".selected");
+                if (selectedParcel) {
+                    selectedParcel.classList.remove("selected");
+                    selectedParcel.style.border = ""
+                    selectedData = ""
+                    selectedIndex = "";
+                }
+                parcelDiv.classList.add("selected");
+                document.querySelector(".selected_parcel").textContent = e.name;
+                selectedData = e;
+                selectedIndex = index;
+                parcelDiv.style.border = "2px dashed rgb(30,134,134)"
+            }
         })
         display_parcel.append(parcelDiv);
     })
+
+    // creation add addition of the last group header because it was not getting added while mapping the data
+    if (currentGroup !== null) {
+        let groupDiv = createHeaderGroup(currentGroup, count);
+        parcel_group.appendChild(groupDiv)
+    }
 }
-renderParcel();
+
+renderParcel(data);
+
+// function and logic to handle the addition of new parcel after selected parcel
+function handleAddAfter() {
+    if (!selectedData) {
+        alert("User needs to select a parcel first")
+    }
+    else {
+        let name = document.querySelector("input").value;
+        let location = document.querySelector("select").value;
+        if (name === "" || location === "") {
+            alert("Please enter Name and Location")
+        } else {
+            const newParcel = {
+                id: data.length + 1,
+                name: name,
+                sequence: selectedData.sequence + 1,
+                group: location
+            }
+            for (let i = selectedIndex + 1; i < copyData.length; i++) {
+                copyData[i].sequence += 1;
+            }
+            copyData.splice(selectedIndex + 1, 0, newParcel);
+            renderParcel(copyData);
+        }
+    }
+}
+
+// function and logic to handle the addition of new parcel before selected parcel
+function handleAddBefore() {
+    if (!selectedData) {
+        alert("User needs to select a parcel first")
+    }
+    else {
+        let name = document.querySelector("input").value;
+        let location = document.querySelector("select").value;
+        if (name === "" || location === "") {
+            alert("Please enter Name and Location")
+        } else {
+            const newParcel = {
+                id: data.length + 1,
+                name: name,
+                sequence: selectedData.sequence,
+                group: location
+            }
+            for (let i = selectedIndex; i < copyData.length; i++) {
+                copyData[i].sequence += 1;
+            }
+            copyData.splice(selectedIndex, 0, newParcel);
+            renderParcel(copyData);
+        }
+    }
+}
+
+// function and logic to handle the replacement of selected parcel
+function handleReplace() {
+    if (!selectedData) {
+        alert("User needs to select a parcel first")
+    }
+    else {
+        let name = document.querySelector("input").value;
+        let location = document.querySelector("select").value;
+        if (name === "" || location === "") {
+            alert("Please enter Name and Location")
+        } else {
+            const newParcel = {
+                id: data.length + 1,
+                name: name,
+                sequence: selectedData.sequence,
+                group: location
+            }
+            copyData.splice(selectedIndex, 1, newParcel);
+            renderParcel(copyData);
+        }
+    }
+}
+
+// function and logic to handle the deletion of selected parcel
+function handleDelete() {
+    if (!selectedData) {
+        alert("User needs to select a parcel first")
+    } else {
+        copyData.splice(selectedIndex, 1);
+        renderParcel(copyData);
+    }
+
+}
+
+// function and logic to show the original data
+function handleRefresh() {
+    renderParcel(data);
+}
+
+// function and logic to display final result after all operations are done on data
+function handleShowFinal() {
+    renderParcel(copyData);
+}
